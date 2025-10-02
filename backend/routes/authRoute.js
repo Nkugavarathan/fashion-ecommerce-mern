8e24bcb2ff5a53988ef4c8fc17a25c63439a4ff7
Nkugavarathan <kugavarathan28@gmail.com>
2025-10-02 17:02:32 +0530

    login and register routes and fn added

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -23,4 +23,20 @@ router.post("/register", async (req, res) => {
     res.status(500).json(err)
   }
 })
+
+//login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username })
+    if (!user) return res.status(404).json("User not found")
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if (!validPassword) return res.status(401).json("Invalid password")
+
+    res.status(200).json(user)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+})
+
 export default router
