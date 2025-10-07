43937bc69ecdde4ab41ecdeec4a14801cef42cd2
Nkugavarathan <kugavarathan28@gmail.com>
2025-10-07 11:43:10 +0530

    order routes updated

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,20 @@
 import express from "express"
-import { createOrder, monthlyIncome } from "./../controllers/orderController"
+import {
+  createOrder,
+  getAll,
+  monthlyIncome,
+  upadateOrder,
+} from "./../controllers/orderController"
 import { admin, protect } from "./../middleware/authMiddleware.js"
 
 const router = express.Router()
 
 router.post("/", protect, createOrder)
+router.put("/:id", upadateOrder)
 router.post("/find/userId", protect, createOrder)
 router.get("/income", monthlyIncome)
+router.delete("/:id", protect, admin)
+
+//get all ordeer
+router.get("/", protect, admin, getAll)
 export default router
