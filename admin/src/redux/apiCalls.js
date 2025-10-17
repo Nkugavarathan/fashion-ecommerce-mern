49cf5ee050244a992a7b48da4f783bffcf72c34e
Nkugavarathan <kugavarathan28@gmail.com>
2025-10-17 10:00:53 +0530

    login successfully added

--- admin/src/redux/apiCalls.js
+++ admin/src/redux/apiCalls.js
@@ -2,11 +2,14 @@ import { loginFailure, loginStart, loginSuccess } from "./userRedux"
 import axios from "axios"
 
 export const login = async (dispatch, user) => {
-  dispatch(loginStart())
+  console.log("Login started...") // 👈 debug log
+  dispatch(loginStart(user))
   try {
     const res = await axios.post("http://localhost:4000/api/auth/login", user)
+    console.log("Login success:", res.data) // 👈 debug log
     dispatch(loginSuccess(res.data))
   } catch (error) {
+    console.error("Login failed:", error) // 👈 debug log
     dispatch(loginFailure())
   }
 }
--- admin/src/redux/store.js
+++ admin/src/redux/store.js
@@ -22,7 +22,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["cart", "user"],
+  whitelist: ["user"],
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
